04e4b72d222d7781a96878b79724d271a2869936
Metick <45372336+Metick@users.noreply.github.com>
2023-10-28 00:25:53 +0200

    DMA Support

--- UEDumper/Memory/driver.h
+++ UEDumper/Memory/driver.h
@@ -5,6 +5,8 @@
 #include <Windows.h>
 #include <tlhelp32.h>
 
+#include <DMALib/DMAHandler.h>
+
 /*
 ██████╗░██╗░░░░░███████╗░█████╗░░██████╗███████╗  ██████╗░███████╗░█████╗░██████╗░██╗
 ██╔══██╗██║░░░░░██╔════╝██╔══██╗██╔════╝██╔════╝  ██╔══██╗██╔════╝██╔══██╗██╔══██╗██║
@@ -14,6 +16,8 @@
 ╚═╝░░░░░╚══════╝╚══════╝╚═╝░░╚═╝╚═════╝░╚══════╝  ╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═════╝░╚═╝
 */
 
+//Global init of DMA
+DMAHandler target;
 
 /// here should be your driver functions
 ///	DO NOT call any of those functions from any class,
@@ -21,20 +25,16 @@
 /// DO NOT include this file in any other file, you might get linker errors!
 /// ANY CHANGES you do to the params in functions, make sure you also edit the memory.cpp and memory.h file!
 
-//global variables here
-HANDLE procHandle = nullptr;
-
 //in case you need to initialize anything BEFORE your com works, you can do this in here.
 //this function IS NOT DESIGNED to already take the process name as input or anything related to the target process
 //use the function "load" below which will contain data about the process name
 inline void init()
 {
+  
+
 	//...
 }
 
-uint64_t getBaseAddress(const wchar_t* processName, int& pid);
-
-void attachToProcess(const int& pid);
 
 /**
  * \brief use this function to initialize the target process
@@ -44,11 +44,17 @@ void attachToProcess(const int& pid);
  */
 inline void loadData(std::string& processName, uint64_t& baseAddress, int& processID)
 {
-    const auto name = std::wstring(processName.begin(), processName.end());
+    const std::wstring str(processName.begin(), processName.end());
+    target.InitializeDMA(str.c_str(), false);
 
-    baseAddress = getBaseAddress(name.c_str(), processID);
+    //not initialized?
+    if (!target.isInitialized())
+        DebugBreak();
 
-    attachToProcess(processID);
+    processID = target.getPID();
+
+    baseAddress = target.getBaseAddress();
+
 }
 
 /**
@@ -60,14 +66,14 @@ inline void loadData(std::string& processName, uint64_t& baseAddress, int& proce
 inline void _read(const void* address, void* buffer, const DWORD64 size)
 {
     size_t bytes_read = 0;
-	BOOL b = ReadProcessMemory(procHandle, address, buffer, size, &bytes_read);
+    BOOL b = target.read(reinterpret_cast<ULONG64>(address), reinterpret_cast<ULONG64>(buffer), size);
     //if failed, try with lower byte amount
     if (!b)
     {
         //always read 10 bytes lower
 	    for(int i = 1; i < size && !b; i+= 10)
 	    {
-            b = ReadProcessMemory(procHandle, address, buffer, size - i, nullptr);
+            b = target.read(reinterpret_cast<ULONG64>(address), reinterpret_cast<ULONG64>(buffer), size - i);
 	    }
     }
 }
@@ -81,60 +87,5 @@ inline void _read(const void* address, void* buffer, const DWORD64 size)
  */
 inline void _write(void* address, const void* buffer, const DWORD64 size)
 {
-	WriteProcessMemory(procHandle, address, buffer, size, nullptr);
-}
-
-
-/**
- * \brief gets the process base address. If you adjust the params, make sure to change them in memory.cpp too
- * \param processName the name of the process
- * \param pid returns the process id
- * \return process base address
- */
-uint64_t getBaseAddress(const wchar_t* processName, int& pid)
-{
-    uint64_t baseAddress = 0;
-	// Get a handle to the process
-    const HANDLE hProcess = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (hProcess == INVALID_HANDLE_VALUE) {
-        return false;
-    }
-
-    // Iterate through the list of processes to find the one with the given filename
-    PROCESSENTRY32 pe32 = { sizeof(PROCESSENTRY32) };
-    if (!Process32First(hProcess, &pe32)) {
-        CloseHandle(hProcess);
-        return false;
-    }
-    while (Process32Next(hProcess, &pe32)) {
-        if (wcscmp(pe32.szExeFile, processName) == 0) {
-            pid = pe32.th32ProcessID;
-            break;
-        }
-    }
-
-    // Get the base address of the process in memory
-    if (pid != 0) {
-        const HANDLE hModule = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
-        if (hModule != INVALID_HANDLE_VALUE) {
-            MODULEENTRY32 me32 = { sizeof(MODULEENTRY32) };
-            if (Module32First(hModule, &me32)) {
-                baseAddress = reinterpret_cast<DWORD64>(me32.modBaseAddr);
-            }
-            CloseHandle(hModule);
-        }
-    }
-    
-    // Clean up and return
-    CloseHandle(hProcess);
-    return baseAddress;
-}
-
-/**
- * \brief this function might not be needed for your driver, this just attaches to the process
- * \param pid process id of the target process
- */
-void attachToProcess(const int& pid)
-{
-	procHandle = OpenProcess(PROCESS_ALL_ACCESS, 0, pid);
+    target.write(reinterpret_cast<ULONG64>(address), reinterpret_cast<ULONG64>(buffer), size);
 }
